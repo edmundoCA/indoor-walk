261315da2802ff6af44ddaf7842e33919b789285
Edmundo Corona <113280885+edmundoCA@users.noreply.github.com>
2025-02-07 20:16:21 -0600

    Implemented stagesMap

--- script.js
+++ script.js
@@ -7,86 +7,72 @@ let isRunning = false;
 let stage = null;
 let speed = null;
 let incline = null;
+const stagesMap = new Map([
+    ["Warm-up", {"speed": 3.5, "incline": 0}],
+    ["Brisk walk", {"speed": 5.5, "incline": 2}],
+    ["High Intensity", {"speed": 6, "incline": 3}],
+    ["Recovery", {"speed": 4, "incline": 1}],
+    ["Steady walk", {"speed": 5, "incline": 2}],
+    ["Cool-down", {"speed": 3.5, "incline": 0}]
+]);
+const stagesArray = Array.from(stagesMap.keys()); 
 
 function mintuesToHundreds(minutes) {
     return minutes * 60 * 100;
 }
 
 function updateUi() {
-    timespanHundreths++;
     updateDisplay();
+    timespanHundreths++;
     if (timespanHundreths < mintuesToHundreds(5))
     {
-        stage = "Warm-up";
-        speed = 3.5;
-        incline = 0;
+        stage = stagesArray[0];
     }
     else if (timespanHundreths < mintuesToHundreds(10))
     {
-        stage = "Brisk walk";
-        speed = 5.5;
-        incline = 2;
+        stage = stagesArray[1];
     }
     else if (timespanHundreths < mintuesToHundreds(11))
     {
-        stage = "High Intensity";
-        speed = 6;
-        incline = 3;
+        stage = stagesArray[2];
     }
     else if (timespanHundreths < mintuesToHundreds(13))
     {
-        stage = "Recovery";
-        speed = 4;
-        incline = 1;
+        stage = stagesArray[3];
     }
     else if (timespanHundreths < mintuesToHundreds(14))
     {
-        stage = "High Intensity";
-        speed = 6;
-        incline = 3;
+        stage = stagesArray[2];
     }
     else if (timespanHundreths < mintuesToHundreds(16))
     {
-        stage = "Recovery";
-        speed = 4;
-        incline = 1;
+        stage = stagesArray[3];
     }
     else if (timespanHundreths < mintuesToHundreds(17))
     {
-        stage = "High Intensity";
-        speed = 6;
-        incline = 3;
+        stage = stagesArray[2];
     }
     else if (timespanHundreths < mintuesToHundreds(19))
     {
-        stage = "Recovery";
-        speed = 4;
-        incline = 1;
+        stage = stagesArray[3];
     }
     else if (timespanHundreths < mintuesToHundreds(20))
     {
-        stage = "High Intensity";
-        speed = 6;
-        incline = 3;
+        stage = stagesArray[2];
     }
     else if (timespanHundreths < mintuesToHundreds(22))
     {
-        stage = "Recovery";
-        speed = 4;
-        incline = 1;
+        stage = stagesArray[3];
     }
     else if (timespanHundreths < mintuesToHundreds(25))
     {
-        stage = "Steady walk";
-        speed = 5;
-        incline = 2;
+        stage = stagesArray[4];
     }
     else if (timespanHundreths < mintuesToHundreds(30))
     {
-        stage = "Cool-down";
-        speed = 3.5;
-        incline = 0;
+        stage = stagesArray[5];
     }
+    ({ speed, incline } = stagesMap.get(stage));
     document.getElementById("stage").innerText = stage;
     document.getElementById("speed").innerText = speed;
     document.getElementById("incline").innerText = incline;
